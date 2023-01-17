e6739b36ecb65f76a52070c8d06e8aa10ffb1446
Bartlomiej Korbiel <korbiel.bartek@gmail.com>
2023-01-17 16:25:53 +0100

    Graph component finished

--- src/App.js
+++ src/App.js
@@ -6,10 +6,28 @@ import './App.css';
 
 function App() {
   const [selectedCurrency, setSelectedCurrency] = useState(null);
+
+  const handleClose = () => {
+    setSelectedCurrency(null);
+  }
   return (
     <div className="App">
       {selectedCurrency ? 
-        <Graph id={selectedCurrency.id} name={selectedCurrency.name}/>
+        <Graph 
+          id={selectedCurrency.id} 
+          name={selectedCurrency.name}
+          image={selectedCurrency.image}
+          current_price={selectedCurrency.current_price}
+          market_cap={selectedCurrency.market_cap}
+          total_volume={selectedCurrency.total_volume}
+          high_24h={selectedCurrency.high_24h}
+          low_24h={selectedCurrency.low_24h}
+          price_change_percentage_24h={selectedCurrency.price_change_percentage_24h}
+          ath={selectedCurrency.ath}
+          ath_date={selectedCurrency.ath_date}
+          market_cap_change_percentage_24h = {selectedCurrency.market_cap_change_percentage_24h}
+          onClose={handleClose}
+        />
         :
         <Exchanges/>
       }
--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -2,20 +2,32 @@ import React, {useState, useEffect} from 'react';
 import Chart from 'react-apexcharts';
 import './graph.css';
 
-const Graph = ({id, name}) => {
+const LoadingSpinner = () => {
+	return (
+    <div className="spinner-container">
+      	<div className="loading-spinner">
+      	</div>
+    </div>
+  );
+}
+
+const Graph = ({id, name, image, current_price, market_cap, total_volume, high_24h, low_24h, price_change_percentage_24h,
+				ath, ath_date, market_cap_change_percentage_24h, onClose}) => {
+
+	const [isLoading, setIsLoading] = useState(false);
 	const [series, setSeries] = useState([]);
 	const [days, setDays] = useState("91");
 
 	const options = {
 		chart: {
-          id: id,
-          type: "line",
-          toolbar: {
-          	tools: {
-          		pan: false
-          	},
-          background: '#fff'
-      	  }
+          	id: id,
+          	type: "line",
+          	toolbar: {
+          		tools: {
+          			pan: false
+          		},
+          		background: '#fff'
+      	  	}
         },
         xaxis: {
             type: 'datetime',
@@ -24,7 +36,7 @@ const Graph = ({id, name}) => {
         	labels:
         	{
 	        	formatter: function (value) {
-			      return value.toFixed(2) + "$";
+			      	return value.toFixed(2) + "$";
 			    }
 			}
         },
@@ -45,6 +57,7 @@ const Graph = ({id, name}) => {
 	}
 
 	useEffect(() => {
+		setIsLoading(true);
 		let timeoutId;
 		async function getLatestData() { 
 	    	try {
@@ -54,9 +67,10 @@ const Graph = ({id, name}) => {
 		    catch (error) {
 		        console.log(error);
 		    }
-		    if (days==1) {
+		    if (days=="1") {
 		    	timeoutId = setTimeout(getLatestData, 1000*60*5);
 		    }
+		    setIsLoading(false);
 	    }
 
 	    getLatestData();
@@ -64,52 +78,71 @@ const Graph = ({id, name}) => {
 	    return () => {
 	    	clearTimeout(timeoutId);
 	    }
-	});
+	}, [id, days]);
 
 	return (
-		<div className="graph-container">
-			<div className="title">
-				{name}
+		<div className="graph-app">
+			{isLoading ? <LoadingSpinner/> : 
+			<div className="graph-container">
+				<div className="title">
+					<img src={image} alt="crypto"/>
+					<div className="name">{name}</div>
+				</div>
+				<div className="info-container">
+					<div className="info"><strong>Current price: </strong>{current_price}$</div>
+					<div className="info"><strong>Volume: </strong>{total_volume}$</div>
+					<div className="info"><strong>Price change 24h: </strong>{price_change_percentage_24h}%</div>
+					<div className="info"><strong>High 24h: </strong>{high_24h}$</div>
+					<div className="info"><strong>Market cap: </strong>{market_cap}$</div>
+					<div className="info"><strong>Low 24h: </strong>{low_24h}$</div>
+					<div className="info"><strong>Market cap change 24h: </strong>{market_cap_change_percentage_24h}%</div>
+					<div className="info"><strong>ATH: </strong>{ath}$ 
+						on {ath_date.replace('T', ', ').slice(0, -5)}
+					</div>
+				</div>
+				<div className="buttons-container">
+		          	<button 
+			            className={["time-button", days=="1" ? "selected-button" : ""].join(" ")} 
+			            onClick={() => setDays("1")}>
+			            1 Day
+		          	</button>
+		          	<button 
+			            className={["time-button", days=="7" ? "selected-button" : ""].join(" ")} 
+			            onClick={() => setDays("7")}>
+			            7 Days
+		          	</button>
+		          	<button 
+			            className={["time-button", days=="30" ? "selected-button" : ""].join(" ")}
+			            onClick={() => setDays("30")}>
+			            1 Month
+		          	</button>
+		          	<button 
+			            className={["time-button", days=="91" ? "selected-button" : ""].join(" ")}
+			            onClick={() => setDays("91")}>
+			            3 Months
+		          	</button>
+		          	<button 
+			            className={["time-button", days=="182" ? "selected-button" : ""].join(" ")} 
+			            onClick={() => setDays("182")}>
+			            6 Months
+		          	</button>
+		          	<button 
+			            className={["time-button", days=="365" ? "selected-button" : ""].join(" ")}
+			            onClick={() => setDays("365")}>
+			            1 Year
+		          	</button>
+		          	<button 
+			            className={["time-button", days=="1826" ? "selected-button" : ""].join(" ")}
+			            onClick={() => setDays("1826")}>
+			            5 Years
+		          	</button>
+		          	<div className="close">
+						<button className="close-button" onClick={onClose}>Close</button>
+					</div>
+			    </div>
+				<Chart options={options} series={series} type="line" width="95%"/>
 			</div>
-
-			<div className="buttons-container">
-	          	<button 
-		            className={["time-button", days=="1" ? "selected-button" : ""].join(" ")} 
-		            onClick={() => setDays("1")}>
-		            1 Day
-	          	</button>
-	          	<button 
-		            className={["time-button", days=="7" ? "selected-button" : ""].join(" ")} 
-		            onClick={() => setDays("7")}>
-		            7 Days
-	          	</button>
-	          	<button 
-		            className={["time-button", days=="30" ? "selected-button" : ""].join(" ")}
-		            onClick={() => setDays("30")}>
-		            1 Month
-	          	</button>
-	          	<button 
-		            className={["time-button", days=="91" ? "selected-button" : ""].join(" ")}
-		            onClick={() => setDays("91")}>
-		            3 Months
-	          	</button>
-	          	<button 
-		            className={["time-button", days=="182" ? "selected-button" : ""].join(" ")} 
-		            onClick={() => setDays("182")}>
-		            6 Months
-	          	</button>
-	          	<button 
-		            className={["time-button", days=="365" ? "selected-button" : ""].join(" ")}
-		            onClick={() => setDays("365")}>
-		            1 Year
-	          	</button>
-	          	<button 
-		            className={["time-button", days=="1826" ? "selected-button" : ""].join(" ")}
-		            onClick={() => setDays("1826")}>
-		            5 Years
-	          	</button>
-		    </div>
-			<Chart options={options} series={series} type="line" width="95%"/>
+			}
 		</div>
 	);
 
